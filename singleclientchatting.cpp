5b6c02889338495e6a2f0ada76741c2c993749cd
Ishan Kumar <ishan.kumar2024@vitstudent.ac.in>
2026-04-13 02:19:05 +0530

    Create singleclientchatting.cpp

---
+++ singleclientchatting.cpp
@@ -0,0 +1,54 @@
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/applications-module.h"
+
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE ("SingleClientChat");
+
+int main (int argc, char *argv[])
+{
+    LogComponentEnable("UdpEchoClientApplication", LOG_LEVEL_INFO);
+    LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);
+
+    NodeContainer nodes;
+    nodes.Create(2); // 1 client, 1 server
+
+    PointToPointHelper p2p;
+    p2p.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
+    p2p.SetChannelAttribute("Delay", StringValue("2ms"));
+
+    NetDeviceContainer devices = p2p.Install(nodes);
+
+    InternetStackHelper stack;
+    stack.Install(nodes);
+
+    Ipv4AddressHelper address;
+    address.SetBase("10.1.1.0", "255.255.255.0");
+
+    Ipv4InterfaceContainer interfaces = address.Assign(devices);
+
+    // Server
+    uint16_t port = 9;
+    UdpEchoServerHelper server(port);
+    ApplicationContainer serverApp = server.Install(nodes.Get(1));
+    serverApp.Start(Seconds(1.0));
+    serverApp.Stop(Seconds(10.0));
+
+    // Client
+    UdpEchoClientHelper client(interfaces.GetAddress(1), port);
+    client.SetAttribute("MaxPackets", UintegerValue(5));
+    client.SetAttribute("Interval", TimeValue(Seconds(1.0)));
+    client.SetAttribute("PacketSize", UintegerValue(1024));
+
+    ApplicationContainer clientApp = client.Install(nodes.Get(0));
+    clientApp.Start(Seconds(2.0));
+    clientApp.Stop(Seconds(10.0));
+
+    Simulator::Run();
+    Simulator::Destroy();
+
+    return 0;
+}
